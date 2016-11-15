3c485bd10dc1457cffe505d7a288fec8728b10ed
Paul Dariye <paul.dariye@gmail.com>
2016-11-15 11:53:55 -0500

    Update scraper tasks to cast type boolean to string and update events controller to filter most recent pr with updated_at propert

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,16 +1,16 @@
 class EventsController < ApplicationController
   def index
-    @events = Event.where.not("repo_url like ?", "%andela%").where("repo_stars >= 2").where(merged: 't')
-    @top_event = Event.where.not("repo_url like ?", "%andela%").where("repo_stars >= 30").where(merged: 't').order('event_created_at DESC').first
+    @events = Event.where.not("repo_url like ?", "%andela%").where("repo_stars >= 2").where(merged: 'true')
+    @top_event = Event.where.not("repo_url like ?", "%andela%").where("repo_stars >= 30").where(merged: "true").order('updated_at DESC').first
     last_updated = @top_event.event_created_at
     last_updated_link = @top_event.event_url
     last_update_made_by = @top_event.user.username
     commits = @events.sum(:commits_count)
     merged = @events.pluck(:repo_url).size
-    contributors = @events.uniq.pluck(:user_id).size
+    contributors = @events.distinct.pluck(:user_id).size
     review_comments = @events.sum(:review_comments)
     languages = @events.where('language IS NOT NULL').group(:language).size 
-    projects = @events.uniq.pluck(:repo_url).size
+    projects = @events.distinct.pluck(:repo_url).size
     commits_by_language = @events.group(:language).sum(:commits_count) 
     render json: { 
       last_updated: last_updated, 
