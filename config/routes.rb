882e2380273437926a849d4f0c0131461baadad5
Paul Dariye <paul.dariye@gmail.com>
2017-04-06 18:52:37 -0400

    Expose all events on endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   root 'events#index'
+  # get 'events/:username', to: 'events#show'
+  get 'events/all', to: 'events#all'
+  # resources :users, only: [:index]
+  # resources :events, only: [:index, :show]
 end
